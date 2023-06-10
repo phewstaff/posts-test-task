2b85ea8db9294439f4be10f36d1a39ac294b14eb
Mohammed <naurbaev2020@gmail.com>
2023-06-10 03:04:17 +0300

    complited selected user ui and logic

--- src/components/business/SelectedUser.tsx
+++ src/components/business/SelectedUser.tsx
@@ -1,7 +1,9 @@
-import React, { FC, useEffect } from "react";
+import { FC, useEffect } from "react";
 import { useParams } from "react-router";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { fetchUserStart } from "../../reduxStore/reducers/selectedUserSlice";
+import { userImage } from "../../assets/const";
+import { Card, Container, Spinner } from "react-bootstrap";
 
 const SelectedUser: FC = () => {
   const { id } = useParams();
@@ -16,8 +18,30 @@ const SelectedUser: FC = () => {
 
   return (
     <>
-      {user && <h1>{user.email}</h1>}
-      {loading && <>loading</>}
+      <Container className="col-md-4 mt-4 text-capitalize">
+        {loading && (
+          <Container className="d-flex min-vh-100 align-items-center mh-100 justify-content-center">
+            <Spinner />
+          </Container>
+        )}
+        {error && <>error: {error}</>}
+        {user && !loading && (
+          <Card className="profile-card">
+            <div className="card-img-block">
+              <Card.Img variant="top" src={userImage} alt="Card image" />
+            </div>
+            <Card.Body>
+              <Card.Title>username: {user.username}</Card.Title>
+              <Card.Title>name: {user.name}</Card.Title>
+              <Card.Text>
+                city: {user.address.city}, street: {user.address.street}, suite:{" "}
+                {user.address.suite}, zipcode: {user.address.zipcode}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )}
+      </Container>
+      <hr />
     </>
   );
 };
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,8 +1,15 @@
 export interface IUser {
   id: number;
+  name: string;
   username: string;
   email: string;
   phone: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
 }
 
 export interface IComment {
